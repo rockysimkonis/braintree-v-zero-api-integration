ee6c937554cdeb6ef009b0c8fa0c32f251e5d8ec
Rocky Simkonis <rokas.simkonis@gmail.com>
2016-07-06 20:12:26 -0500

    Adding routes, controllers, and views

---
+++ braintree-api-integration/app/controllers/welcome_controller.rb
@@ -0,0 +1,5 @@
+class WelcomeController < ActionController::Base
+  def index
+    #code
+  end
+end
--- braintree-api-integration/app/models/user.rb
+++ braintree-api-integration/app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   has_many :carts
   has_many :products, through: :carts
+
+  has_secure_password
+
+  validates :email, presence: true
+  validates_uniqueness_of :email
+
+
+
 end
